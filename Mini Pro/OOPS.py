a0229d291670220f5f8f0e76c63883fd33c6fa2c
HBJ17 <hbj17672007@gmail.com>
2025-11-03 06:16:37 +0530

    added few files

---
+++ Mini Pro/OOPS.py
@@ -0,0 +1,22 @@
+#object oriented programming language
+
+class car:
+    def __init__(self, brand, model):
+        self.brand = brand  
+        self.model = model
+    
+    def show_details(self):
+        print(f"Car : {self.brand} , {self.model}")
+
+car1 = car("Scoda","Rapid")
+car2 = car("Audi","A30")
+
+car1.show_details()
+car2.show_details()
+
+
+
+
+
+
+
