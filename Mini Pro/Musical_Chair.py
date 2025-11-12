8ca8fbb3f556fbe329c17fc246f293636c1887f7
HBJ17 <hbj17672007@gmail.com>
2025-11-12 11:06:11 +0530

    added musical chair file

---
+++ Mini Pro/Musical_Chair.py
@@ -0,0 +1,39 @@
+import random
+import time
+
+def musical_chairs():
+    print(" Welcome to the Musical Chairs Game! ")
+    players = []
+
+    n = int(input("Enter the number of players: "))
+    for i in range(1, n + 1):
+        name = input(f"Enter player {i} name: ")
+        players.append(name)
+
+    print()
+    print("Let's start the game!")
+
+    r = 1
+
+   
+    while len(players) > 1:
+        print()
+        print(f"    Round {r}    ")
+        print("Music is playing... ")
+        time.sleep(random.randint(2, 10))  
+
+        print("Music stopped!")
+
+        e = random.choice(players)
+        players.remove(e)
+        print(f"{e} couldn't find a chair and is eliminated!")
+
+        print(f"Players left: {', '.join(players)}")
+        r += 1
+        time.sleep(2)
+
+    print()
+    print("The winner is:", players[0], "! Congratulations!")
+
+
+musical_chairs()
