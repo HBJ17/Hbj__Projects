9ef25c220091daa9aa629d903b097dfbca8668fa
HBJ17 <hbj17672007@gmail.com>
2025-12-30 07:37:38 +0530

    generate_password function was created

--- Mini Pro/password_generator.py
+++ Mini Pro/password_generator.py
@@ -1,4 +1,16 @@
 import random
 import string
 
+def generate_password(length):
+    password = [
+        random.choice(string.ascii_letters),
+        random.choice(string.digits),
+        random.choice(string.punctuation)
+    ]
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password += random.choices(characters, k=length-3)
+    
+    random.shuffle(password)
+    return ''.join(password)
 
