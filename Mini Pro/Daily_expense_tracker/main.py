7725224bdb765ee3a2fc058da1b34d0357d568c2
HBJ17 <hbj17672007@gmail.com>
2026-01-15 12:00:00 +0530

    Added expense saving using file handling

--- Mini Pro/Daily_expense_tracker/main.py
+++ Mini Pro/Daily_expense_tracker/main.py
@@ -14,3 +14,16 @@ while True:
         break
     else:
         print("Invalid choice")
+
+def add_expense():
+    item = input("Item name: ")
+    amount = input("Amount: ")
+
+    with open("expenses.txt", "a") as file:
+        file.write(item + "," + amount + "\n")
+
+    print("Expense saved!")
+
+# connect to menu
+if choice == "1":
+    add_expense()
