b7a151ce1d71872f03679b552bbb8683467fcd5f
HBJ17 <hbj17672007@gmail.com>
2026-01-16 12:00:00 +0530

    Added expense viewing and total calculation

--- Mini Pro/Daily_expense_tracker/main.py
+++ Mini Pro/Daily_expense_tracker/main.py
@@ -1,7 +1,9 @@
 def show_menu():
     print("\n--- Expense Tracker ---")
     print("1. Add Expense")
-    print("2. Exit")
+    print("2. View Expenses")
+    print("3. Exit")
+
 
 while True:
     show_menu()
@@ -27,3 +29,19 @@ def add_expense():
 # connect to menu
 if choice == "1":
     add_expense()
+
+def view_expenses():
+    total = 0
+
+    try:
+        with open("expenses.txt", "r") as file:
+            print("\n--- Expenses ---")
+            for line in file:
+                item, amount = line.strip().split(",")
+                print(item, ":", amount)
+                total += int(amount)
+
+        print("Total Spent:", total)
+    except:
+        print("No expenses found!")
+
