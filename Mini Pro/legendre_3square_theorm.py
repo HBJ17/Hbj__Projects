58b6c4aa75aa897de0b3a95bc89136bedb9809f2
HBJ17 <hbj17672007@gmail.com>
2026-01-18 12:00:00 +0530

    made a word count program

---
+++ Mini Pro/legendre_3square_theorm.py
@@ -0,0 +1,21 @@
+def factors(n):
+    if n == 0:
+        return([0])
+    factor_list = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            factor_list.append(i)
+    return(factor_list)
+
+def square(n):
+    return(len(factors(n))%2 == 1)
+
+def three_squares(n):
+    for i in range(0,n+1):
+        for j in range(i,n+1):
+            if square(i) and square(j) and square(n-(i+j)):
+                return(
+                    True)
+    return(False)
+
+three_squares(8)
